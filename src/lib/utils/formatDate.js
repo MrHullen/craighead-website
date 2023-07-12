de0f4f2d8174042b600d14b0e73ce67387962f76
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2023-07-12 13:26:34 +1200

    Update formatDate.js

--- src/lib/utils/formatDate.js
+++ src/lib/utils/formatDate.js
@@ -1,7 +1,7 @@
 export function formatDate(date) {
-	return new Date(date).toLocaleDateString('en-US', {
-		month: 'long',
+	return new Date(date).toLocaleDateString('en-NZ', {
 		day: 'numeric',
+		month: 'long',
 		year: 'numeric'
 	})
 }
