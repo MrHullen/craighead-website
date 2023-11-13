556b3be86c2d07fa008880e36233da25ec90d424
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2023-11-14 09:50:33 +1300

    Reformats newsletter and moves head to component

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,21 +1,13 @@
 <script>
+  import Head from '$lib/Head.svelte'
   import InfoPage from '$lib/subpages/InfoPage.svelte'
-  import { urlFor } from '$lib/utils/image'
 
   export let data
-
-  // get the favicon
-  import { getContext } from 'svelte'
-  let logo = getContext('logo')
 </script>
 
-<svelte:head>
-  <title>About Us</title>
-  <meta name="description" content="Girls who attend Craighead are known for their grace, spirit, character and high levels of intrinsic motivation. They are confident, connected and caring global citizens. They desire to be the best they can be, yet retain the values of respect, integrity, courtesy, cooperation and kindness." />
-  <meta property="og:title" content="Craighead Diocesan School - About Us" />
-  <meta property="og:url" content="https://craighead.school.nz/about-us" />
-  <meta property="og:description" content="Girls who attend Craighead are known for their grace, spirit, character and high levels of intrinsic motivation. They are confident, connected and caring global citizens. They desire to be the best they can be, yet retain the values of respect, integrity, courtesy, cooperation and kindness." />
-  <meta property="og:image" content={urlFor(logo).auto('format').bg('fff').url()} />
-</svelte:head>
+<Head
+  title="About Us"
+  description="Girls who attend Craighead are known for their grace, spirit, character and high levels of intrinsic motivation. They are confident, connected and caring global citizens. They desire to be the best they can be, yet retain the values of respect, integrity, courtesy, cooperation and kindness."
+  />
 
 <InfoPage content={data.content} />
--- src/routes/newsletter/[date]/+page.svelte
+++ src/routes/newsletter/[date]/+page.svelte
@@ -1,41 +1,40 @@
 <script>
-	import { PortableText } from '@portabletext/svelte'
-	import { formatDate } from '$lib/utils/formatDate'
-	import { urlFor } from '$lib/utils/image'
+  import Head from '$lib/Head.svelte'
   import Header from '$lib/subpages/Header.svelte'
   import ArticlePreview from '$lib/newsletter/ArticlePreview.svelte'
+	import { PortableText } from '@portabletext/svelte'
+	import { formatDate } from '$lib/utils/formatDate'
   import { page } from '$app/stores'
 
-	export let data
+  export let data
 </script>
 
-<Header title="Kōrero Newsletter" subtitle="{formatDate($page.params.date)}" />
+<Head
+  title="Kōrero"
+  description="Kōrero is Craighead's weekly newsletter. The Principal includes a short messages about what's going on, key dates and notices are posted, and there are articles about events and student achievements."
+  />
 
-<div class="columns content section has-background-white">
-  <div class="column is-one-third">
-    <section class="image">
-      <img src="/principal.jpeg" alt="The Principal of Craighead Diocesan School" />
-    </section>
-    
-    <section class="content section">
-      <h2> Notices </h2>
-      <PortableText value={data.newsletter.notices} />
-    </section>
-    
-    <section class="content section">
-      <h2> Dates </h2>
-      <PortableText value={data.newsletter.dates} />
-    </section>
-  </div>
-  <div class="column">
-    <section class="content section principals-message">
-      <h2>Principal's Message</h2>
-      <PortableText value={data.newsletter.principalsAddress} />
-    </section>
-  </div>
-</div>
+<Header title="Kōrero" subtitle="{formatDate($page.params.date)}" />
 
-<section class="content section">
+<main class="content section has-background-white">
+  <h2 class="principals-title">Principal's Message</h2>
+  <img class="image" src="/principal.jpeg" alt="The Principal of Craighead Diocesan School" />
+  <div class="principals-message">
+    <PortableText value={data.newsletter.principalsAddress} />
+  </div>
+
+  <article class="notices has-background-white">
+    <h2> Notices </h2>
+    <PortableText value={data.newsletter.notices} />
+  </article>
+
+  <article class="dates has-background-white">
+    <h2> Dates </h2>
+    <PortableText value={data.newsletter.dates} />
+  </article>
+</main>
+
+<section class="content section has-background-white">
   <h2> Articles </h2>
   {#if data.newsletter?.articles?.length}
     {#each data.newsletter?.articles as article}
@@ -47,11 +46,30 @@
 </section>
 
 <style>
-  section {
-    background-color: #fff;
+  main {
+    display: grid;
+    gap: 2em;
+    align-items: start;
+    grid: " p-t p-t " min-content
+          " i   p-m " min-content
+          " n   p-m " min-content
+          " d   p-m " min-content
+          " .   p-m " min-content
+          /1fr  2fr ;
   }
 
-  .principals-message {
-    padding-top: 0;
+  .principals-title { grid-area: p-t; }
+  .image { grid-area: i; }
+  .principals-message { grid-area: p-m; }
+  .notices { grid-area: n; }
+  .dates { grid-area: d; }
+
+  /* mobile */
+  @media (max-width: 700px) {
+    main {
+      display: flex;
+      flex-direction: column;
+      gap: 2em;
+    }
   }
 </style>
