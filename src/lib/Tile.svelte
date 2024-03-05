e038f3d4bbeb048bd572432198e0109c7971833b
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2024-03-06 09:56:18 +1300

    Updates landing page.

---
+++ src/lib/Tile.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { urlFor } from '$lib/utils/image'
+
+  export let title
+  export let link
+  export let data
+</script>
+
+<a href={link}>
+  <img src="{urlFor(data).width(300).height(200).url()}" alt="{data.caption}" />
+  <span>
+    <h3>{title}</h3>
+  </span>
+</a>
+
+<style>
+  a { 
+    position: relative;
+    height: 200px;
+  }
+
+  span {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background: rgba(255, 255, 255, 0.9);
+  }
+  
+  h3 {
+    margin: 0.5em 1em; 
+  }
+</style>
---
+++ src/lib/Video.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { browser } from '$app/environment'
+  import '@mux/mux-video'
+
+  export let playbackId
+</script>
+
+<!-- video only works in browser (can't be SSR) -->
+{#if browser}
+  <mux-video playback-id={playbackId} autoplay loop muted></mux-video>
+{/if}
+
+<style>
+  mux-video {
+    width: 100%;
+  }
+</style>
