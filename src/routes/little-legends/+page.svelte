635291691005424ab5185228e66f5c2d0631d889
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2024-03-28 14:25:08 +1300

    Create +page.svelte

---
+++ src/routes/little-legends/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Head from '$lib/Head.svelte'
+</script>
+
+<Head
+  title="Little Legends"
+  />
+
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSetQ_Qk-Qnb7puCOBMxlVRmucFdarr1DmErZcpInLhPQeljbw/viewform?embedded=true" width="640" height="1691" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
+  <style>
+    iframe {
+      margin: 0 auto;
+      display: block;
+    }
+  </style>
